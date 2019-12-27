458456388f831da094feb1549afb83f6dbe94c2c
chzhu <zhuch.1990@gmail.com>
2019-12-27 23:24:07 +0800

    Solve 17

---
+++ 17.电话号码的字母组合.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode.cn id=17 lang=python
+#
+# [17] 电话号码的字母组合
+#
+
+# @lc code=start
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        mapping = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        results = ['']
+        for d in digits:
+            temp = []
+            for r in results:
+                temp.extend([r + c for c in mapping[d]])
+            results = temp
+        return results
+        
+# @lc code=end
+
